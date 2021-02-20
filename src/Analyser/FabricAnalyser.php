68fc44407a5a7c1149dd4b6010c1c7445efca66a
Julian <julian@aternos.org>
2021-02-20 22:27:46 +0100

    Revert "remove FabricVanillaVersionInformation"
    This reverts commit 6464bf477ec38e8825309ed7137f0017ac8c03a9.

--- src/Analyser/FabricAnalyser.php
+++ src/Analyser/FabricAnalyser.php
@@ -3,7 +3,9 @@
 namespace Aternos\Codex\Minecraft\Analyser;
 
 use Aternos\Codex\Minecraft\Analysis\Information\Fabric\FabricJavaVersionInformation;
+use Aternos\Codex\Minecraft\Analysis\Information\Fabric\FabricVanillaVersionInformation;
 use Aternos\Codex\Minecraft\Analysis\Information\Fabric\FabricVersionInformation;
+use Aternos\Codex\Minecraft\Analysis\Information\Vanilla\VanillaVersionInformation;
 use Aternos\Codex\Minecraft\Analysis\Problem\Fabric\FabricModDependencyProblem;
 
 /**
@@ -17,6 +19,7 @@ class FabricAnalyser extends VanillaAnalyser
     {
         parent::__construct();
         $this->addPossibleInsightClass(FabricVersionInformation::class);
+        $this->overridePossibleInsightClass(VanillaVersionInformation::class, FabricVanillaVersionInformation::class);
         $this->addPossibleInsightClass(FabricJavaVersionInformation::class);
         $this->addPossibleInsightClass(FabricModDependencyProblem::class);
     }
