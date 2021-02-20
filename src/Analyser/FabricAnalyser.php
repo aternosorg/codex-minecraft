6464bf477ec38e8825309ed7137f0017ac8c03a9
Julian <julian@aternos.org>
2021-02-20 21:48:15 +0100

    remove FabricVanillaVersionInformation

--- src/Analyser/FabricAnalyser.php
+++ src/Analyser/FabricAnalyser.php
@@ -3,9 +3,7 @@
 namespace Aternos\Codex\Minecraft\Analyser;
 
 use Aternos\Codex\Minecraft\Analysis\Information\Fabric\FabricJavaVersionInformation;
-use Aternos\Codex\Minecraft\Analysis\Information\Fabric\FabricVanillaVersionInformation;
 use Aternos\Codex\Minecraft\Analysis\Information\Fabric\FabricVersionInformation;
-use Aternos\Codex\Minecraft\Analysis\Information\Vanilla\VanillaVersionInformation;
 use Aternos\Codex\Minecraft\Analysis\Problem\Fabric\FabricModDependencyProblem;
 
 /**
@@ -19,7 +17,6 @@ class FabricAnalyser extends VanillaAnalyser
     {
         parent::__construct();
         $this->addPossibleInsightClass(FabricVersionInformation::class);
-        $this->overridePossibleInsightClass(VanillaVersionInformation::class, FabricVanillaVersionInformation::class);
         $this->addPossibleInsightClass(FabricJavaVersionInformation::class);
         $this->addPossibleInsightClass(FabricModDependencyProblem::class);
     }
