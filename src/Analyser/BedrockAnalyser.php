d4a5e7b9711fae182628aba07325ec31486e73b7
Julian <julian@aternos.org>
2022-03-02 15:45:30 +0100

    add new version of withelist without online authentication error

--- src/Analyser/BedrockAnalyser.php
+++ src/Analyser/BedrockAnalyser.php
@@ -3,6 +3,7 @@
 namespace Aternos\Codex\Minecraft\Analyser;
 
 use Aternos\Codex\Minecraft\Analysis\Information\Bedrock\BedrockVersionInformation;
+use Aternos\Codex\Minecraft\Analysis\Problem\Bedrock\BedrockAuthenticationAllowlistProblem;
 use Aternos\Codex\Minecraft\Analysis\Problem\Bedrock\BedrockAuthenticationWhitelistProblem;
 use Aternos\Codex\Minecraft\Analysis\Problem\Bedrock\BedrockDBStorageChainProblem;
 
@@ -17,6 +18,7 @@ class BedrockAnalyser extends MinecraftAnalyser
     {
         $this->addPossibleInsightClass(BedrockVersionInformation::class);
         $this->addPossibleInsightClass(BedrockAuthenticationWhitelistProblem::class);
+        $this->addPossibleInsightClass(BedrockAuthenticationAllowlistProblem::class);
         $this->addPossibleInsightClass(BedrockDBStorageChainProblem::class);
     }
 }
---
+++ src/Analysis/Solution/Bedrock/DisableAllowlistSolution.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Aternos\Codex\Minecraft\Analysis\Solution\Bedrock;
+
+use Aternos\Codex\Minecraft\Translator\Translator;
+
+/**
+ * Class DisableAllowlistSolution
+ *
+ * @package Aternos\Codex\Minecraft\Analysis\Solution\Bedrock
+ */
+class DisableAllowlistSolution extends BedrockSolution
+{
+    /**
+     * Get the solution as a human readable message
+     *
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return Translator::getInstance()->getTranslation("disable-allowlist-solution");
+    }
+}
