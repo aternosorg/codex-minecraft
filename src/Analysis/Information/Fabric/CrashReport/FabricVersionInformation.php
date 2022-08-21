d49c3e832c60c589281237855b433123be56ee8d
Julian <julian@aternos.org>
2022-08-21 16:29:56 +0200

    detect fabric version in crash reports

---
+++ src/Analysis/Information/Fabric/CrashReport/FabricVersionInformation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Aternos\Codex\Minecraft\Analysis\Information\Fabric\CrashReport;
+
+class FabricVersionInformation extends \Aternos\Codex\Minecraft\Analysis\Information\Fabric\FabricVersionInformation
+{
+    public static function getPatterns(): array
+    {
+        return [
+            '/\t\tfabricloader: Fabric Loader ('. static::$fabricVersionPattern .')/'
+        ];
+    }
+
+    /**
+     * @param string[] $matches
+     * @param mixed $patternKey
+     * @return void
+     */
+    public function setMatches(array $matches, mixed $patternKey): void
+    {
+        $this->value = $matches[1];
+    }
+}
