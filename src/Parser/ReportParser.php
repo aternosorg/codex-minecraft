8bdb9b356b3c61299b7efa90d9f12d63fbf8e9f7
Paul Vogel <paul@aternos.org>
2025-10-13 16:13:55 +0200

    use ReportLevel enum in ReportParser

--- src/Parser/ReportParser.php
+++ src/Parser/ReportParser.php
@@ -7,23 +7,18 @@ use Aternos\Codex\Minecraft\Log\Entry;
 
 class ReportParser extends \Aternos\Codex\Parser\Parser
 {
-    protected const string LEVEL_INFO = "info";
-    protected const string LEVEL_WARNING = "warn";
-    protected const string LEVEL_TITLE = "title";
-    protected const string LEVEL_COMMENT = "comment";
-    protected const string LEVEL_STACKTRACE = "stacktrace";
 
-    protected const string DEFAULT_LEVEL = self::LEVEL_INFO;
+    protected const ReportLevel DEFAULT_LEVEL = ReportLevel::INFO;
 
     /**
      * @var array<string, array<string>> Regular expression patterns to match log lines to levels
      */
     protected const array PATTERN = [
-        self::LEVEL_WARNING => ["/\s*WARNING: .*$/"],
-        self::LEVEL_STACKTRACE => ["/^\s+at (?:\w+\/+)?\S+\(.+\)$/", "/^(?:Caused by: )?[a-z]+\.(?:\w+\.?)+: .*$/", "/^\\tat [a-z]+\..*$/"],
-        self::LEVEL_INFO => ["/^([^|:]+:) .+$/", "/^(\w+:)$/", "/^(\\t[^:.]+:)$/"],
-        self::LEVEL_TITLE => ["/^-{2,4} .* -{2,4}$/"],
-        self::LEVEL_COMMENT => ["/\/\/.*$/"],
+        ReportLevel::WARNING->value => ["/\s*WARNING: .*$/"],
+        ReportLevel::STACKTRACE->value => ["/^\s+at (?:\w+\/+)?\S+\(.+\)$/", "/^(?:Caused by: )?[a-z]+\.(?:\w+\.?)+: .*$/", "/^\\tat [a-z]+\..*$/"],
+        ReportLevel::INFO->value => ["/^([^|:]+:) .+$/", "/^(\w+:)$/", "/^(\\t[^:.]+:)$/"],
+        ReportLevel::TITLE->value => ["/^-{2,4} .* -{2,4}$/"],
+        ReportLevel::COMMENT->value => ["/\/\/.*$/"],
     ];
 
     /**
@@ -46,7 +41,7 @@ class ReportParser extends \Aternos\Codex\Parser\Parser
                         }
                         continue 3;
                     }
-                    $entry->setLevel(ReportLevel::fromString(static::DEFAULT_LEVEL));
+                    $entry->setLevel(static::DEFAULT_LEVEL);
                 }
             }
         }
