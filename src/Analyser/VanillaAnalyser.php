2215abfe89326d188242269e352fc92f267b1858
Paul Vogel <paul@aternos.org>
2025-11-19 16:44:05 +0100

    add CodeOfConductFolderMissingProblem to VanillaAnalyser

--- src/Analyser/VanillaAnalyser.php
+++ src/Analyser/VanillaAnalyser.php
@@ -5,6 +5,7 @@ namespace Aternos\Codex\Minecraft\Analyser;
 use Aternos\Codex\Minecraft\Analysis\Information\Vanilla\VanillaVersionInformation;
 use Aternos\Codex\Minecraft\Analysis\Problem\Vanilla\AquaticWorldOnOlderVersionProblem;
 use Aternos\Codex\Minecraft\Analysis\Problem\Vanilla\AuthServerProblem;
+use Aternos\Codex\Minecraft\Analysis\Problem\Vanilla\CodeOfConductFolderMissingProblem;
 use Aternos\Codex\Minecraft\Analysis\Problem\Vanilla\MalformedEncodingProblem;
 use Aternos\Codex\Minecraft\Analysis\Problem\Vanilla\OldPlayerDirectoryProblem;
 use Aternos\Codex\Minecraft\Analysis\Problem\Vanilla\OverworldSettingsMissingProblem;
@@ -14,13 +15,13 @@ class VanillaAnalyser extends MinecraftAnalyser
 {
     public function __construct()
     {
-        $this->addPossibleInsightClass(VanillaVersionInformation::class);
-
-        $this->addPossibleInsightClass(OldPlayerDirectoryProblem::class);
         $this->addPossibleInsightClass(AquaticWorldOnOlderVersionProblem::class);
-        $this->addPossibleInsightClass(TickingBlockEntityProblem::class);
-        $this->addPossibleInsightClass(MalformedEncodingProblem::class);
         $this->addPossibleInsightClass(AuthServerProblem::class);
+        $this->addPossibleInsightClass(CodeOfConductFolderMissingProblem::class);
+        $this->addPossibleInsightClass(MalformedEncodingProblem::class);
+        $this->addPossibleInsightClass(OldPlayerDirectoryProblem::class);
         $this->addPossibleInsightClass(OverworldSettingsMissingProblem::class);
+        $this->addPossibleInsightClass(TickingBlockEntityProblem::class);
+        $this->addPossibleInsightClass(VanillaVersionInformation::class);
     }
 }
