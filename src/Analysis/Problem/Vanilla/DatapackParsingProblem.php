06b0ce29cd2020dad0ff31ff3956e2d646733051
Paul Vogel <paul@aternos.org>
2026-03-30 18:27:58 +0200

    detect problem when parsing of datapack fails

---
+++ src/Analysis/Problem/Vanilla/DatapackParsingProblem.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Aternos\Codex\Minecraft\Analysis\Problem\Vanilla;
+
+use Aternos\Codex\Minecraft\Analysis\Solution\File\FileDeleteSolution;
+use Aternos\Codex\Minecraft\Translator\Translator;
+
+class DatapackParsingProblem extends VanillaProblem
+{
+    protected string $datapack;
+
+    /**
+     * @inheritDoc
+     */
+    public function getMessage(): string
+    {
+        return Translator::getInstance()->getTranslation("datapack-parsing-problem");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getPatterns(): array
+    {
+        return ['/java.lang.IllegalStateException: Failed to parse .+ from pack file\/(.*)/'];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setMatches(array $matches, mixed $patternKey): void
+    {
+        $this->datapack = $matches[1];
+        $this->addSolution(new FileDeleteSolution($this->datapack));
+    }
+}
